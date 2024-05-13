86a65cae56a14ff4b6d123305ab9136d3e5ba737
NicoGrandoli <163182208+HijoDeHonor@users.noreply.github.com>
2024-05-13 07:51:24 -0300

    eliminacion de comentarios innecesarios

--- game-store-fe/src/services/loginService.js
+++ game-store-fe/src/services/loginService.js
@@ -1,10 +1,8 @@
 import users from "../assets/utils/users.json";
 
-const logInService = /*async*/ (User, setError) => {
-  // try {
-  //   const res = await fetch(`/api/users/${User.username}`);
-  //   const data = await res.json();
-  const data = users; // change this line for the line above
+const logInService = (User, setError) => {
+  
+  const data = users; 
   const userFound = data.find((u) => u.username === User.username && u.password === User.password);
   if (userFound) {
     localStorage.setItem("token", userFound.token);
@@ -15,10 +13,7 @@ const logInService = /*async*/ (User, setError) => {
 
     return setError("Usuario o contraseña incorrectos");
   }
-  // } catch (error) {
-  //   console.error(error);
-  //   setError("Error al iniciar sesión");
-  // }
+
 };
 
 export default logInService;
--- game-store-fe/src/services/registerService.js
+++ game-store-fe/src/services/registerService.js
@@ -1,18 +1,5 @@
 import users from "../assets/utils/users.json";
-const checkUsernameExists = /*async*/ (newUsername) => {
-  //   try {
-  //   const response = await fetch(`/api/users/${newUsername}`);
-  //   const data = await response.json();
-  //   if (response.status === 404) {
-  //     return false; // El nombre de usuario no existe
-  //   } else if (response.ok) {
-  //     return true; // El nombre de usuario ya existe
-  //   } else {
-  //     throw new Error("Error al verificar el nombre de usuario: " + data.error);
-  //   }
-  //   } catch (error) {
-  //   console.error("Error al verificar el nombre de usuario:", error);
-  //   return true;
+const checkUsernameExists =(newUsername) => {
   const exists = users.find((user) => user.username === newUsername);
   if (exists) {
     return true;
@@ -21,38 +8,8 @@ const checkUsernameExists = /*async*/ (newUsername) => {
   }
 };
 
-const handleSubmit = /*async*/ ( newUser, setError) => {
-    // event.preventDefault();
-    // const exists = checkUsernameExists(newUser.username);
-    // if (exists) {
-    //     setError("Nombre de usuario no disponible");
-    //     return;
-    // }
-    // if (newUser.password !== newUser.confirmPassword) {
-    //     setError("Las contraseñas no coinciden.");
-    //     return;
-    // }
-    // {
-    //     try {
-    //         const res = await fetch('/api/users', {
-    //             method: 'POST',
-    //             headers: {
-    //                 'Content-Type': 'application/json'
-    //             },
-    //             body: JSON.stringify(newUser)
-    //         });
-    //         if (!res.ok) {
-    //             throw new Error('Error al registrar usuario');
-    //         }{
-    //         const data = await res.json();
-    //         console.log(data);
-    //         alert("Registro exitoso");
-    //         }
-    //     } catch (error) {
-    //         console.error(error);
-    //         setError("Error al registrar usuario");
-    //     }
-    // }
+const handleSubmit = ( newUser, setError) => {
+    newUser.token = "token user" + (users.length + 1);
     const exists = checkUsernameExists(newUser.username);
     if (exists) {
         setError("Nombre de usuario no disponible");
@@ -62,6 +19,7 @@ const handleSubmit = /*async*/ ( newUser, setError) => {
         setError("Las contraseñas no coinciden.");
         return; 
     }
+    users.push(newUser);
     alert("Registro exitoso, Bienvenido a Game Store");
 };
 
